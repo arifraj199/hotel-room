1c9e5a69c087ba259d69b04d1f6662d0da4ab571
arifraj199 <karif9514@gmail.com>
2023-01-31 14:58:43 +0600

    initial commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChBUdioHqDnXBh9cY3_M-ATdrYS3ROmE0",
+  authDomain: "hotel-room-76ef2.firebaseapp.com",
+  projectId: "hotel-room-76ef2",
+  storageBucket: "hotel-room-76ef2.appspot.com",
+  messagingSenderId: "588577059914",
+  appId: "1:588577059914:web:34340dfe02643db0dc22b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
